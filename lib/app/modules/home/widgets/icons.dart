56e2a777f120bc0a5afa1bdf319414ad90e2000d
Dadah Taufik <dadahtaufik@refactory.id>
2023-03-13 03:53:08 +0700

    add Task

--- lib/app/modules/home/widgets/icons.dart
+++ lib/app/modules/home/widgets/icons.dart
@@ -1,15 +1,16 @@
 
 import 'package:flutter/cupertino.dart';
 
+import '../../../core/values/colors.dart';
 import '../../../core/values/icons.dart';
 
-List<Icon> getIcons(){
+List<Icon> getIcons (){
   return const [
-    Icon(IconData(personIcon, fontFamily: 'MaterialIcons')),
-    Icon(IconData(workIcon, fontFamily: 'MaterialIcons')),
-    Icon(IconData(movieIcon, fontFamily: 'MaterialIcons')),
-    Icon(IconData(sportIcon, fontFamily: 'MaterialIcons')),
-    Icon(IconData(travelIcon, fontFamily: 'MaterialIcons')),
-    Icon(IconData(shopIcon, fontFamily: 'MaterialIcons')),
+    Icon(IconData(personIcon, fontFamily: 'MaterialIcons'), color: purple),
+    Icon(IconData(workIcon, fontFamily: 'MaterialIcons'),color: pink),
+    Icon(IconData(movieIcon, fontFamily: 'MaterialIcons'),color: green),
+    Icon(IconData(sportIcon, fontFamily: 'MaterialIcons'),color: yellow),
+    Icon(IconData(travelIcon, fontFamily: 'MaterialIcons'),color: deepPink),
+    Icon(IconData(shopIcon, fontFamily: 'MaterialIcons'),color: lightBlue),
   ];
 }
