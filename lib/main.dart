7ed31879defe6499660ba26672a0ee01a8b3bd0b
Dadah Taufik <dadahtaufik@refactory.id>
2023-03-12 21:51:17 +0700

    add extension

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:todo_list_get_x/app/data/services/service.dart';
 
 import 'app/routes/app_pages.dart';
+import 'package:get_storage/get_storage.dart';
 
-void main() {
+void main() async {
+  await GetStorage.init();
+  await Get.putAsync(() => StorageService().init());
+
   runApp(
     GetMaterialApp(
-      title: "Application",
+      title: "Todo List using GetX",
       initialRoute: AppPages.INITIAL,
       getPages: AppPages.routes,
     ),
